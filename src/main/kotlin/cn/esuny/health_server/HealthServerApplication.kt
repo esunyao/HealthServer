23c3c586b7fcf9239d1e19eefc8ec54158be762a
Esuny <esun.yao@hotmail.com>
2026-07-26 12:57:42 +0800

    Rename DietServer to HealthServer
    Rebrand the application from DietServer to HealthServer. This includes renaming the main application class, test class, and updating the project description in build.gradle.

--- src/main/kotlin/cn/esuny/health_server/HealthServerApplication.kt
+++ src/main/kotlin/cn/esuny/health_server/HealthServerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DietServerApplication
+class HealthServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<DietServerApplication>(*args)
+    runApplication<HealthServerApplication>(*args)
 }
--- src/test/kotlin/cn/esuny/health_server/HealthServerApplicationTests.kt
+++ src/test/kotlin/cn/esuny/health_server/HealthServerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DietServerApplicationTests {
+class HealthServerApplicationTests {
 
     @Test
     fun contextLoads() {
